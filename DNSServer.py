eb855d56e37d60f87fb23220e2ce9203db2e0f40
aseazzu <as14927@nyu.edu>
2025-03-22 17:07:16 -0400

    fixed the data to reques :facepalm:

--- DNSServer.py
+++ DNSServer.py
@@ -117,7 +117,7 @@ def run_dns_server():
             # Parse the request using the `dns.message.from_wire` method
             request = dns.message.from_wire(data)
             # Create a response message using the `dns.message.make_response` method
-            response = dns.message.make_response(data)
+            response = dns.message.make_response(request)
 
             # Get the question from the request
             question = request.question[0]
@@ -135,8 +135,8 @@ def run_dns_server():
                     for pref, server in answer_data:
                         rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
                 elif qtype == dns.rdatatype.SOA:
-                    mname, rname, serial, refresh, rety, expire, minimum = answer_data  # What is the record format? See dns_records dictionary. Assume we handle @, Class, TTL elsewhere. Do some research on SOA Records
-                    rdata = SOA(dns.rdataclass.IN, dns.rdatatype.SOA, mname, rname, serial, refresh, rety, expire,
+                    mname, rname, serial, refresh, retry, expire, minimum = answer_data  # What is the record format? See dns_records dictionary. Assume we handle @, Class, TTL elsewhere. Do some research on SOA Records
+                    rdata = SOA(dns.rdataclass.IN, dns.rdatatype.SOA, mname, rname, serial, refresh, retry, expire,
                                 minimum)  # follow format from previous line
                     rdata_list.append(rdata)
                 else:
